3c983c2955c9f7070511c9ac3419f7b5201e8d7d
mycaelli <mycaelli@usp.br>
2022-04-12 21:19:54 +0200

    libft.h working

--- libft.h
+++ libft.h
@@ -3,7 +3,11 @@
 //Even though, isalpha() takes integer as an argument, character is passed to isalpha() function.
 #ifndef LIBFT_H
 
-#define LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
 
 int	ft_isalpha(int c);
 
@@ -13,12 +17,30 @@ int	ft_isalnum(int c);
 
 int	ft_isascii(int c);
 
-int ft_isprint(int c);
+int     ft_isprint(int c);
 
 int	ft_tolower(int c);
 
 int	ft_toupper(int c);
 
 void	*ft_memset(void *block, int c, size_t size);
+
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+void	*ft_memmove(void *dest, const void *src, size_t n);
+
+void	*ft_memchr(const void *block, int c, size_t size);
+
+int	ft_memcmp(const void* a1, const void* a2, size_t size);
+
+size_t ft_strlen(const char *s);
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
+char *ft_strchr(const char *s, int c);
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif

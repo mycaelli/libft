30521a2aad8913976d2992fa4f4de02aacc8c5de
mycaelli <mycaelli@usp.br>
2022-04-14 23:48:24 +0200

    failed test 05 strlibTester

---
+++ ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 21:55:54 by mcerquei          #+#    #+#             */
+/*   Updated: 2022/04/14 23:14:56 by mcerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char *sub;
+    unsigned int i;
+
+    i = 0;
+    sub = NULL;
+    sub = malloc((len + 1) * sizeof(char));
+    if (!sub)
+        return (NULL);
+    while (i < len && s[start + i])
+    {
+        sub[i] = s[start + i];
+        i++;
+    }
+    sub[i] = '\0';
+    return (sub);
+}

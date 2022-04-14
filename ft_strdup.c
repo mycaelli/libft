63a46566a4fac0271ff92ba3eb87e9c1db30392f
mycaelli <mycaelli@usp.br>
2022-04-14 21:47:59 +0200

    failed libftTester 02 and 04

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,33 +6,31 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 00:01:27 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/04/14 17:41:41 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/04/14 21:47:12 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-char *ft_strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-    char *cpy;
-    int i;
-    int j;
+	char	*cpy;
+	int		i;
+	int		j;
 
-    i = 0;
-    j = 0;
-    cpy = NULL;
-    while (s[i])
-        i++;
-   // if (!*s || !s)
-   //     return (NULL); 
-    cpy = malloc(i+1 * sizeof(char));
-    if (!cpy)
-        return (NULL);
-    while (j <= i)
-    {
-        cpy[j] = s[j];
-        j++;
-    }
-    return (cpy);
+	i = 0;
+	j = 0;
+	cpy = NULL;
+	while (s[i])
+		i++;
+	cpy = malloc(i + 1 * sizeof(char));
+	if (!cpy)
+		return (NULL);
+	while (j <= i)
+	{
+		cpy[j] = s[j];
+		j++;
+	}
+	return (cpy);
 }

42ffdbb7fe3567bc6e59cabb2f105029e1d625b9
mycaelli <mycaelli@usp.br>
2022-04-18 14:29:18 +0200

    ft_substr OK

--- ft_substr.c
+++ ft_substr.c
@@ -6,27 +6,69 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 21:55:54 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/04/14 23:14:56 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/04/18 14:28:53 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+size_t	ft_strlen_(const char *s)
 {
-    char *sub;
-    unsigned int i;
+	size_t	i;
 
-    i = 0;
-    sub = NULL;
-    sub = malloc((len + 1) * sizeof(char));
-    if (!sub)
-        return (NULL);
-    while (i < len && s[start + i])
-    {
-        sub[i] = s[start + i];
-        i++;
-    }
-    sub[i] = '\0';
-    return (sub);
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup_(const char *s)
+{
+	char	*cpy;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	cpy = NULL;
+	while (s[i])
+		i++;
+	cpy = malloc(i + 1 * sizeof(char));
+	if (!cpy)
+		return (NULL);
+	while (j <= i)
+	{
+		cpy[j] = s[j];
+		j++;
+	}
+	return (cpy);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*sub;
+	unsigned int	i;
+	unsigned int	s_len;
+	unsigned int	sub_len;
+
+	s_len = ft_strlen_(s);
+	i = 0;
+	sub = NULL;
+	if (start > s_len)
+		return (ft_strdup_(""));
+	sub_len = ft_strlen_(&s[start]);
+	if (len > sub_len)
+		len = sub_len;
+	sub = malloc((len + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	while (i < len && s[start + i])
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
 }

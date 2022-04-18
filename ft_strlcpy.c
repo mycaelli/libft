a4810adfa5f4c39a1241eeac9e920e302c905f41
mycaelli <mycaelli@usp.br>
2022-04-18 15:33:15 +0200

    update ft_strlcpy OK

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 22:37:29 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/04/12 20:23:31 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/04/18 15:32:03 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	len;
 
 	i = 0;
-	len = 0;
+	len = 0; 
+        while (src[len])
+		len++;
+	if (!size)
+            return (len);
 	while ((i < size - 1) && (size > 0) && src[i])
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
-	while (src[len])
-		len++;
 	return (len);
 }

1331ac41b5f7553c50069d731f826097582ef2c6
mycaelli <mycaelli@usp.br>
2022-04-18 21:24:29 +0200

    ft_strjoin OK

---
+++ ft_strjoin.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 14:33:59 by mcerquei          #+#    #+#             */
+/*   Updated: 2022/04/18 21:24:05 by mcerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_slen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*concat;
+
+	i = 0;
+	j = 0;
+	concat = malloc((ft_slen(s1) + ft_slen(s2) + 1) * sizeof(char));
+	if (!concat)
+		return (NULL);
+	while (s1[i])
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		concat[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (concat);
+}

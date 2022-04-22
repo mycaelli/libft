b2eb8106268ccef655fbd215086fda76cb992f05
mycaelli <mycaelli@usp.br>
2022-04-23 00:17:53 +0200

    ft_strmapi OK

---
+++ ft_strmapi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 23:33:32 by mcerquei          #+#    #+#             */
+/*   Updated: 2022/04/23 00:15:28 by mcerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int ft_len(char const *s)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char *str;
+    int len;
+    int i;
+    
+    i = 0;
+    len = ft_len(s);
+    str = (char *)malloc((len + 1) * sizeof(char));
+    if (!str)
+        return (NULL);
+    while (s[i])
+    {
+        str[i] = f(i, s[i]);
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}

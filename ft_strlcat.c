fe4d896ae6063359e8fafad24682c53e4a2adeec
mycaelli <mycaelli@usp.br>
2022-04-24 01:41:32 +0200

    in progress NOT OK

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,15 +6,38 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 00:10:13 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/04/18 21:04:06 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/04/24 01:30:51 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include "stdio.h"
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	dst_len;
+	
+	size_t	i;
+	size_t	j;
+	size_t	dest_length;
+	size_t	src_length;
+
+	src_length = ft_strlen(src);
+	dest_length = ft_strlen(dst);
+	j = dest_length;
+	i = 0;
+	if (dest_length < size - 1 && size > 0)
+	{
+		while (src[i] && dest_length + i < size - 1)
+		{
+			dst[j] = src[i];
+			j++;
+			i++;
+		}
+		dst[j] = 0;
+	}
+	if (dest_length >= size)
+		dest_length = size;
+	return (dest_length + src_length);
+/*	size_t	dst_len;
 	size_t	src_len;
 	size_t	i;
 	size_t	j;
@@ -30,12 +53,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	}
 	while (src[src_len])
 		src_len++;
-	if ((long) size < 0)
-		return (dst_len + src_len);
-	if (size < dst_len + 1)
+//	if ((long) size < 0)
+//        {
+//                printf("entrou\n");
+//		return (dst_len + src_len);
+//	}
+        if (size < dst_len + 1)
 		return (size + src_len);
-	while (i < size - 1)
+	while (i < size - 1 || (long) size < 0)
 		dst[i++] = src[j++];
 	dst[i] = '\0';
 	return (dst_len + src_len);
+*/
 }

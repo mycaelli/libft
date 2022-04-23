b14a6ea4ac32a599a3a29135b43e6502eea469cd
mycaelli <mycaelli@usp.br>
2022-04-23 22:26:44 +0200

    ft_strrchr OK

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 22:01:38 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/04/12 22:41:41 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/04/23 22:25:59 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strrchr(const char *string, int c)
 	i = 0;
 	while (string[i])
 		i++;
+	if (c == 0 && string[i] == 0)
+		return ((char *) &string[i]);
 	while (i--)
 	{
 		if (string[i] == (unsigned char) c)

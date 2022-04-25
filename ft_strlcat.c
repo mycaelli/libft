536bd896916e3de461fb597e0687afe483bc7b1e
mycaelli <mycaelli@usp.br>
2022-04-25 10:41:27 +0200

    ft_strlcat OK

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,27 +6,36 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 00:10:13 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/04/24 01:30:51 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/04/25 10:41:03 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stdio.h"
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+
+static size_t	ft_len_(char *str)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{	
 	size_t	i;
 	size_t	j;
-	size_t	dest_length;
+	size_t	dst_length;
 	size_t	src_length;
 
-	src_length = ft_strlen(src);
-	dest_length = ft_strlen(dst);
-	j = dest_length;
+	dst_length = ft_len_(dst);
+	src_length = ft_len_((char *) src);
+	j = dst_length;
 	i = 0;
-	if (dest_length < size - 1 && size > 0)
+	if (dst_length < size - 1 && size > 0)
 	{
-		while (src[i] && dest_length + i < size - 1)
+		while (src[i] && dst_length + i < size - 1)
 		{
 			dst[j] = src[i];
 			j++;
@@ -34,35 +43,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 		}
 		dst[j] = 0;
 	}
-	if (dest_length >= size)
-		dest_length = size;
-	return (dest_length + src_length);
-/*	size_t	dst_len;
-	size_t	src_len;
-	size_t	i;
-	size_t	j;
-
-	j = 0;
-	i = 0;
-	dst_len = 0;
-	src_len = 0;
-	while (dst[dst_len])
-	{
-		dst_len++;
-		i++;
-	}
-	while (src[src_len])
-		src_len++;
-//	if ((long) size < 0)
-//        {
-//                printf("entrou\n");
-//		return (dst_len + src_len);
-//	}
-        if (size < dst_len + 1)
-		return (size + src_len);
-	while (i < size - 1 || (long) size < 0)
-		dst[i++] = src[j++];
-	dst[i] = '\0';
-	return (dst_len + src_len);
-*/
+	if (dst_length >= size)
+		dst_length = size;
+	return (dst_length + src_length);
 }

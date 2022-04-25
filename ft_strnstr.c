f2da12bae5cdd15b6ef151d179d691d0b932e701
mycaelli <mycaelli@usp.br>
2022-04-25 12:06:29 +0200

    ft_strnstr OK

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,14 +6,14 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 01:03:21 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/04/13 02:42:04 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/04/25 12:03:20 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-int	ft_strncmp_(const char *a1, const char *a2, size_t size)
+static int	ft_strncmp_(const char *a1, const char *a2, size_t size)
 {
 	const char	*s1;
 	const char	*s2;
@@ -37,7 +37,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	l_len = 0;
 	if (!big)
 		return (0);
-	if (!little)
+	if (!little[0])
 		return ((char *) big);
 	while (little[l_len])
 		l_len++;

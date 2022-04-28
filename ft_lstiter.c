395bb0b4e6ef375ba34e7972b17aa08090265dcb
mycaelli <mycaelli@usp.br>
2022-04-28 12:06:09 +0200

    ft_lstiter OK

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -1,8 +1,22 @@
-void ft_lstiter(t_list *lst, void (*f)(void *))
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 05:45:56 by mcerquei          #+#    #+#             */
+/*   Updated: 2022/04/28 12:04:08 by mcerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	while (lst->next)
+	while (lst)
 	{
-		(*f)(lst->content);
+		f(lst->content);
 		lst = lst->next;
 	}
 }

01297b090009ba7101b73fdeecf185219904ac36
mycaelli <mycaelli@usp.br>
2022-04-28 07:10:01 +0200

    ft_lstnew OK

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,10 +1,25 @@
-t_list *ft_lstnew(void *content)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 05:47:00 by mcerquei          #+#    #+#             */
+/*   Updated: 2022/04/28 05:47:19 by mcerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h" 
+
+t_list	*ft_lstnew(void *content)
 {
-	t_list *lst;
-	
-	lst = (t_list *)malloc(1 * sizeof(*t_list));
+	t_list	*lst;
+
+	lst = (t_list *)malloc(1 * sizeof(t_list));
 	if (!lst)
-	 return (NULL);
+		return (NULL);
 	lst->content = content;
-	lst->next = NULL;	
+	lst->next = NULL;
+	return (lst);
 }

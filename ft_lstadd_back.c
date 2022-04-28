c1b91185640157890e4ef8543392a5a7feee9149
mycaelli <mycaelli@usp.br>
2022-04-28 10:01:54 +0200

    ft_lstadd_back OK -> NORM NOT WORKING

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,8 +1,28 @@
-void ft_lstadd_back(t_list **lst, t_list *new)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 05:43:52 by mcerquei          #+#    #+#             */
+/*   Updated: 2022/04/28 05:44:08 by mcerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	//caso da lista estar vazia??
-	while (*lst->next)
-		*lst = *lst->next;
-	*lst->next = new;
-	new->next = NULL;
+	t_list	*last;
+
+	last = *lst;
+	if (!(*lst))
+		*lst = new;
+	else
+	{
+		while (last->next)
+			last = last->next;
+		last->next = new;
+	}
 }

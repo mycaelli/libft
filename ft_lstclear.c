dfc3cbf8e6bb26725b78c90b5888738ada4b0bbc
mycaelli <mycaelli@usp.br>
2022-04-28 12:02:43 +0200

    ft_lstclear OK

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,12 +1,26 @@
-void ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	t_list *temp;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 05:45:10 by mcerquei          #+#    #+#             */
+/*   Updated: 2022/04/28 12:00:01 by mcerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	temp = lst;
-	while (*lst->next)
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	temp = *lst;
+	while ((*lst))
 	{
-		temp = *lst->next;
-		(*del)(*lst);
+		temp = (*lst)->next;
+		del((*lst)->content);
 		free(*lst);
 		*lst = temp;
 	}

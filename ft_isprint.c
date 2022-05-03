85ef096e8211677d39e292e7f5384d9f20ab7b59
mycaelli <mycaelli@usp.br>
2022-05-03 05:17:53 +0200

    mouli appproved

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,13 +6,13 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 21:57:10 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/04/05 22:11:51 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/04/30 23:01:45 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isprint(int c)
 {
-	if ((c >= 0 && c <= 31) || c == 127)
-		return (0);
-	return (16384);
+	if (c >= 32 && c <= 126)
+		return (16384);
+	return (0);
 }
